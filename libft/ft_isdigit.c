521a7cecb259623814fbc9cc45a675338db354b2
Seongjin Seo <seonseo@c8r4s14.42seoul.kr>
2023-10-12 20:42:07 +0900

    ft_isdigit.c

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -1,33 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seonseo <seonseo@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/06 11:23:38 by seonseo           #+#    #+#             */
-/*   Updated: 2023/10/12 20:39:59 by seonseo          ###   ########.fr       */
+/*   Created: 2023/10/12 20:40:47 by seonseo           #+#    #+#             */
+/*   Updated: 2023/10/12 20:41:42 by seonseo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalpha(int c)
+int	ft_isdigit(int c)
 {
-	if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'))
-		return (1);
-	return (0);
+	
 }
-/*
-#include <stdio.h>
-
-int	main(void)
-{
-	char	c;
-
-	c = 0;
-	while (scanf("%c", &c))
-	{
-		printf("%d\n", ft_isalpha(c));
-		getchar();
-	}
-}
-*/
