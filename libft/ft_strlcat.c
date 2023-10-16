894ab2a05f5f0f1ef139c77548b6887e231d6f26
Seongjin Seo <seonseo@c3r6s5.42seoul.kr>
2023-10-16 21:16:54 +0900

    ft_toupper.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: seonseo <seonseo@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 16:27:56 by seonseo           #+#    #+#             */
-/*   Updated: 2023/10/16 17:30:32 by seonseo          ###   ########.fr       */
+/*   Updated: 2023/10/16 21:05:09 by seonseo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,66 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 	dstlen = ft_strlen(dst);
 	srclen = ft_strlen(src);
-	i = dstlen;
-	j = 0;
-	while (i < dstsize - 1)
+	if (dstsize <= dstlen)
+		return (dstsize + srclen);
+	else if (dstlen < dstsize - 1)
 	{
-		dst[i] = src[j];
-		i++;
-		j++;
+		i = dstlen;
+		j = 0;
+		while (src[j] && i < dstsize - 1)
+		{
+			dst[i] = src[j];
+			i++;
+			j++;
+		}
+		dst[i] = '\0';
 	}
-	dst[i] = '\0';
-	return ();
+	return (dstlen + srclen);
 }
+
+#include <string.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char			dest[42] = "apple";
+	char			src[] = "pie";
+	size_t			len;
+	
+	len = ft_strlcat(dest, src, 42);
+	printf("%s %zu\n", dest, len);
+	return 0;
+}
+
+/*
+int	main(void)
+{
+	char arr[50];
+
+	for (int i = 0; i < 50; ++i)
+		arr[i] = '#';
+	arr[0] = '\0';
+	int len = ft_strlcat(arr, "", 50);
+	printf("%s %d\n", arr, len);
+	len = ft_strlcat(arr, "Hello ", 50);
+	printf("%s %d\n", arr, len);
+	len = ft_strlcat(arr, "World!", 50);
+	printf("%s %d\n", arr, len);
+	len = ft_strlcat(arr, "", 50);
+	printf("%s %d\n", arr, len);
+	len = ft_strlcat(arr, "42", 0);
+	printf("%s %d\n", arr, len);
+	len = ft_strlcat(arr, "42", 3);
+	printf("%s %d\n", arr, len);
+	len = ft_strlcat(arr, "\nHello World!", 20);
+	printf("%s %d\n", arr, len);
+	len = ft_strlcat(arr, "World!", 20);
+	printf("%s %d\n", arr, len);
+	len = ft_strlcat(arr, "\nHello World!", 42);
+	printf("%s %d\n", arr, len);
+	for (int i = 31; i < 42; ++i)
+		printf("%d ", arr[i]);
+	printf("\n");
+	return 0;
+}
+*/
